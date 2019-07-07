5bbc904e8e37556709af374bb72323f672c2487c
SanitarskiyDima <43505027+SanitarskiyDima@users.noreply.github.com>
2019-07-07 14:21:26 +0300

    Update WritingPricesTest.java

--- src/test/java/Test/WritingPricesTest.java
+++ src/test/java/Test/WritingPricesTest.java
@@ -9,13 +9,13 @@ import java.util.ArrayList;
 public class WritingPricesTest extends BaseTest {
     @Test
     public void writingPrices() throws IOException, InterruptedException {
-//        sleep(2000);
-//        mainPage.getAllPrices();
-//        mainPage.getPricesToFile();
+        sleep(2000);
+        mainPage.getAllPrices();
+        mainPage.getPricesToFile();
         ArrayList<String> files1 = new ArrayList<>();
         files1.add( "/home/sanitarskiy/IdeaProjects/QALight_PricesToFile_PageObject/prices.txt");
         files1.add( "/home/sanitarskiy/IdeaProjects/QALight_PricesToFile_PageObject/pom.xml");
-//        MailSender1.sendFromFile("sanitarskiy7@gmail.com", "123QWEqwe", "this message is for test attach files", files1);
+        MailSender1.sendFromFile("sanitarskiy7@gmail.com", "123QWEqwe", "this message is for test attach files", files1);
         MailSender.mailFromfile("emails.txt", "123QWEqwe", "this message is for test attach files", files1);
  
     }
